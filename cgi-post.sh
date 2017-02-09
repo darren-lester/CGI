69194e77205197b8123c2ce383d4ed329af2c61b
Darren Lester <darrenlester@live.co.uk>
2017-02-09 11:36:03 +0000

    Do not set QUERY_STRING

--- cgi-post.sh
+++ cgi-post.sh
@@ -2,13 +2,12 @@
 
 # Script for testing POST requests to CGI programs
 # $1 is the CGI program
-# $2 is the query string
+# $2 is the request body
 # $3 is the path info
 
 export REQUEST_METHOD=POST
 export CONTENT_TYPE=application/x-www-form-urlencoded
-export QUERY_STRING=$2
-export CONTENT_LENGTH=`echo -n $QUERY_STRING | wc -c`
+export CONTENT_LENGTH=`echo -n $2 | wc -c`
 export PATH_INFO=$3
 
 echo $2 | $1
